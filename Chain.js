85dd09def0b6a7e89067246b208c5bdcf03c8680
shail9090 <83891379+shail9090@users.noreply.github.com>
2021-10-10 14:01:45 -0700

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,14 @@
+class Chain{
+    constructor(body1,body2){
+        var options = {
+            bodyA:body1,
+            bodyB:body2,
+            length:50
+        }
+        this.chain= Constraint.create(options);
+        World.add(world, chain);
+    }
+    display(){
+    line(this.chain.bodyA.position.x, this.chain.bodyA.position.y, this.chain.bodyB.position.x, this.chain.bodyB.position.y);
+    }
+}
